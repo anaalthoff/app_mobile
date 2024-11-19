9abc234d9c470f270a875d643ec42e1a71a76f37
Ana Paula Althoff <ana.althoff@srssistemas.com>
2024-11-19 18:54:46 -0300

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.5.1" // Updated AGP version
+        id("org.jetbrains.kotlin.android") version "1.9.10" // Updated Kotlin plugin version
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Gym"
+include(":app")
+ 
