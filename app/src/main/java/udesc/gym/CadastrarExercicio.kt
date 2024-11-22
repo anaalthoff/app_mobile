e8a551b1d33e1c58b1ab4aa1879944e1a21f44a8
Ana Paula Althoff <ana.althoff@srssistemas.com>
2024-11-22 11:46:31 -0300

    feat: ícones adicionados e layouts corrigidos.'

--- app/src/main/java/udesc/gym/CadastrarExercicio.kt
+++ app/src/main/java/udesc/gym/CadastrarExercicio.kt
@@ -3,13 +3,11 @@ package udesc.gym
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CadastroExercicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro_exercicio)
+        setContentView(R.layout.activity_cadastrar_exercicio)
     }
 }
