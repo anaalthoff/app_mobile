948001b1621b25b789afa5ad90395b4a344c57f6
Ana Paula Althoff <ana.althoff@srssistemas.com>
2024-11-22 17:37:02 -0300

    feat: tela inicial finalizada. Botão abre nova intent.

--- app/src/main/java/udesc/gym/MainActivity.kt
+++ app/src/main/java/udesc/gym/MainActivity.kt
@@ -1,15 +1,30 @@
 package udesc.gym
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import udesc.gym.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.main)
+
+        binding.buttonContinuar.setOnClickListener {
+            // intent é o recurso de mudar de tela
+            // cria um objeto da classe Intent, diz a tela inicial e a final, informando a classe que implementa essa activity
+            var intent = Intent(this, Painel::class.java)
+            // chama o método abaixo e passa a intent
+            startActivity(intent)
+        }
     }
 }
